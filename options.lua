51e6905e125af55bea2833db822789c3d995abf6
Kelly Crabbé <kelly.crabbe@gmail.com>
2014-03-01 01:19:58 +0100

    added checkboxes

--- options.lua
+++ options.lua
@@ -19,7 +19,7 @@ local defaults = {
    statsoutline = nil, -- nil, OUTLINE, THICKOUTLINE, MONOCHROMEOUTLINE
    statssize = 14,
    statsfont = STANDARD_TEXT_FONT,
-   framelock = true,
+   locked = true,
    shadowcolor = {0, 0, 0, .8} -- keeping it universal
 }
 
@@ -33,17 +33,33 @@ Panel:Hide()
 ------------------------------------------------------------------------
 -- GUI methods
 ------------------------------------------------------------------------
-function Panel:AddCheckbox(name, text, point, anchor, rpoint)
-   local checkButton = CreateFrame("Button", name, Panel, "UICheckButtonTemplate")
-   checkButton.text = name.."Text"
-   checkButton.text:SetText(text)
+function Panel:AddCheckbox(parent, text, table, key)
+   local checkButton = CreateFrame("Button", nil, parent, "UICheckButtonTemplate")
+
+   local label = checkButton:CreateFontString(nil, 'ARTWORK', "GameFontHighlight")
+   label:SetPoint("LEFT", checkButton, "RIGHT", 2, 1)
+   label:SetText(text)
+   checkButton.label = label
+
+   -- hook functions into this shithole of doom
+   checkButton:SetScript("OnClick",
+      function (self, checkButton, down)
+         if ( checkButton:GetChecked() ) then
+            PlaySound("igMainMenuOptionCheckBoxOn")
+         else
+            PlaySound("igMainMenuOptionCheckBoxOff")
+         end
+         -- InterfaceOptionsPanel_CheckButton_OnClick(self);
+      end)
+
+   return checkButton
 end
 
 function Panel:AddColorPicker(name, text, desc, point, anchor, rpoint)
 end
 
-function Panel:CreateSlider(name, text, parent, low, high, step)
-   local slider = CreateFrame('Slider', name, parent, 'OptionsSliderTemplate')
+function Panel:CreateSlider(name, text, low, high, step)
+   local slider = CreateFrame('Slider', name, 'Panel', 'OptionsSliderTemplate')
    slider:SetScript('OnMouseWheel', Slider_OnMouseWheel)
    slider:SetMinMaxValues(low, high)
    slider:SetValueStep(step)
@@ -56,6 +72,16 @@ function Panel:CreateSlider(name, text, parent, low, high, step)
    text:SetPoint('LEFT', slider, 'RIGHT', 7, 0)
    slider.valText = text
    return slider
+end
+
+function Panel:CreateButton(name, text)
+   local button = CreateFrame('Button', name, 'Panel', 'UIPanelButtonTemplate')
+   button.text:SetText(text)
+   local text = button:CreateFontString(nil, 'BACKGROUND')
+   text:SetFontObject('GameFontHighlightSmall')
+   text:SetPoint('LEFT', button, 'RIGHT', 7, 0)
+   button.valText = text
+   return button
 end
 
 -----------------------------
@@ -102,9 +128,46 @@ Panel:SetScript('OnShow', function(self)
    Description:SetText('Just for Bob, we wrote The Bob Clock!')
    self.Description = Description
 
-   -- local Color = self:CreateFontString(nil, nil, 'GameFontNormal')
-   local timeFormatButton = Panel:AddCheckbox('TimeFormatBox', 'Check to enable 24h format', 'LEFT', timeFormat, 'RIGHT')
-   -- timeFormatButton:SetScript("OnClick", function() end)
+   -- Clock group
+   local ClockSettings = self:CreateFontString(nil, nil, 'GameFontNormal')
+   ClockSettings:SetPoint('TOPLEFT', Description, 'BOTTOMLEFT', 0, -10)
+   ClockSettings:SetPoint('RIGHT', -32, 0)
+   ClockSettings:SetJustifyH('LEFT')
+   ClockSettings:SetText('Clock Settings')
+   self.ClockSettings = ClockSettings
+
+   -- time format
+   local timeFormatButton = Panel:AddCheckbox(Panel, "Check to enable 24h format.", BobTheClockDB, timeform24)
+   timeFormatButton:SetPoint("TOPLEFT", ClockSettings, "BOTTOMLEFT", 0, -5)
+
+   -- enable shadow
+   local enDisShadow = Panel:AddCheckbox(Panel, "Check to enable a shadow on the clock.", BobTheClockDB, clockshadow)
+   enDisShadow:SetPoint("TOPLEFT", timeFormatButton, "BOTTOMLEFT", 0, -5)
+
+   -- outline
+   local enOutline = Panel:AddCheckbox(Panel, "Check to enable an outline on the clock.", BobTheClockDB, clockoutline)
+   enOutline:SetPoint("TOPLEFT", enDisShadow, "BOTTOMLEFT", 0, -5)
+
+   -- Stats group
+   local StatsStettings = self:CreateFontString(nil, nil, 'GameFontNormal')
+   StatsStettings:SetPoint('TOPLEFT', enOutline, 'BOTTOMLEFT', 0, -10)
+   StatsStettings:SetPoint('RIGHT', -32, 0)
+   StatsStettings:SetJustifyH('LEFT')
+   StatsStettings:SetText('Stats Settings')
+   self.StatsStettings = StatsStettings
+
+   -- show?
+   local showStats = Panel:AddCheckbox(Panel, "Check to show stats (FPS, latency and memory)", BobTheClockDB, statsshow)
+   showStats:SetPoint("TOPLEFT", StatsStettings, "BOTTOMLEFT", 0, -5)
+
+   -- shadow
+   local shadowstats = Panel:AddCheckbox(Panel, "Check to show a shadow on the stats.", BobTheClockDB, statsshadow)
+   shadowstats:SetPoint("TOPLEFT", showStats, "BOTTOMLEFT", 0, -5)
+
+   -- outline
+   local outlinestats = Panel:AddCheckbox(Panel, "Check to show an outline around the stats.", BobTheClockDB, statsoutline)
+   outlinestats:SetPoint("TOPLEFT", shadowstats, "BOTTOMLEFT", 0, -5)
+
 
    self:SetScript('OnShow', nil)
 end)
